1f9f34be3019efb8459b9c6291db4aea97e47661
GOEL Deeptanshu <deeptanshu.goel@soprasteria.com>
2020-05-26 14:27:06 +0530

    Requirements added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api, request
+from flask_restful import Resource, Api, request, reqparse
 from tasks import Task, TaskStatus
 from flask_jwt import JWT, jwt_required
 from authenticate import authenticate, identity
@@ -18,7 +18,7 @@ tsks = [
 
 
 class task(Resource):
-    @jwt_required()
+    # @jwt_required()
     def get(self):
         return {'tasks': tsks}, 200
 
@@ -39,14 +39,20 @@ class task(Resource):
         return {'message': 'Can not find task'}, 400
 
     def delete(self):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('_id',
+                            type=int,
+                            required=True,
+                            help='Need a ID in ')
+        data = parser.parse_args()
+
+        # data = request.get_json()
         _id = data.get('_id', None)
         for tsk in tsks:
             if tsk['id'] == _id:
                 tsks.remove(tsk)
                 return {'message': 'Task removed'}, 202
         return {'message': 'Can not find task'}, 400
-
 
 
 api.add_resource(task, '/tasks')
